5361bc6fbc1c1740122de23cf63a884bfb92b64f
vamsi-rebounz <vamsi.cheruku@rebounz.org>
2025-07-24 12:54:47 -0500

    Remove Firebase key from tracking

--- config/firebase.js
+++ config/firebase.js
@@ -1,7 +1,8 @@
 const { initializeApp } = require('firebase/app');
 const { getStorage } = require('firebase/storage');
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+// const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 // Firebase config for client SDK
 const firebaseConfig = {
